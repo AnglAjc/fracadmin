11ac4647d8f691caeee738b9736554d3adefe2b0
AnglAjc <andrieez25@gmail.com>
2026-06-03 23:02:22 -0600

    v12: fix

--- fracadmin/frontend/admin/src/pages/MorososPage.jsx
+++ fracadmin/frontend/admin/src/pages/MorososPage.jsx
@@ -206,6 +206,7 @@ export default function MorososPage() {
                             </button>
                           </div>
                         </td>
+                      </tr>
                     ))}
                   </tbody>
                 </table>
